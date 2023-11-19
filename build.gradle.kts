54ac04830a62bad34b23176258dd6ad393c89d19
velinken <velinken@gmail.com>
2023-11-19 15:31:34 +0300

    бетта 26

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 // Файл сборки верхнего уровня, в который вы можете добавить параметры конфигурации, общие для всех подпроектов/модулей.
 plugins {
     id("com.android.application") version "8.2.0-rc03" apply false
+//    id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     //id ("androidx.navigation.safeargs.kotlin")  version "2.7.5" apply false
     //id ("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
