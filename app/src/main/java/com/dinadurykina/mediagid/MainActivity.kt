bf804339be7022e50f7eda50213bc46353d373b8
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-11-19 17:52:10 +0300

    API 26-34

--- app/src/main/java/com/dinadurykina/mediagid/MainActivity.kt
+++ app/src/main/java/com/dinadurykina/mediagid/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.dinadurykina.mediagid.databinding.ActivityMainBinding
+//import com.dinadurykina.mediagid.ui.Play
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 
@@ -28,20 +29,24 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
+       // Play.init(this)
+
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+
+         //   Play.start("rus_2_1")
             /*
             val afd = assets.openFd("1_1_rus.mp3")
             val player = MediaPlayer()
             player.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
             player.prepare()
             player.start()
-
+            */
             val resID = resources.getIdentifier("rus_1_1", "raw", packageName)
             val mediaPlayer = MediaPlayer.create(this, resID)
             mediaPlayer.start()
-            */
+
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
@@ -57,6 +62,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/dinadurykina/mediagid/ui/utils.kt
@@ -0,0 +1,26 @@
+package com.dinadurykina.mediagid.ui
+
+import android.app.Activity
+import android.media.MediaPlayer
+/*
+object Play {
+    lateinit var  mediaPlayer : MediaPlayer
+    lateinit var activity: Activity
+    lateinit var name: String
+    fun init(activity: Activity) {
+        this.activity = activity
+
+    }
+    fun start(name: String) {
+        this.name = name
+        val resID = activity.resources.getIdentifier(
+            name,
+            "raw",
+            activity.packageName
+        )
+        mediaPlayer = MediaPlayer.create(activity, resID)
+        mediaPlayer.start()
+    }
+}
+
+ */
